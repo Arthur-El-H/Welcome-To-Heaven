c2c3424a0e813140a3e3503b714396db39bf0e8e
Arthur <arthur.el-hussein@student.uni-halle.de>
2021-06-13 13:11:03 +0200

    Update Stairway.cs

--- Stairway.cs
+++ Stairway.cs
@@ -32,8 +32,16 @@ public class Stairway : MonoBehaviour
         //Play Handshake-Animation for player and Regular
         if (waiters[index].CheckHolyness())
         {
-            player.MoveToPos(positions[index]);
-            waiters[index].MoveToPos(positions[player.currentPositionIndex]);
+            changePos(waiters[index], index++);
+            changePos(player, index);
         }
+    }
+
+    private void changePos(AbstractWaiter waiter, int index)
+    {
+        waiter.MoveToPos(positions[index]);  //physical Movement
+        waiters[waiter.currentPositionIndex] = null; //set current Pos to null, should be irrelevant, because it is either filled in the next step or never used again
+        waiters[index] = waiter;             //change index in List
+        waiter.currentPositionIndex = index; //change index-reference
     }
 }
